b5ce2a47bde6f7b33d8463028172740e1d594159
Linus Färnstrand <linus@mullvad.net>
2017-09-26 10:52:16 +0200

    Add result constants

--- oqs-sys/src/lib.rs
+++ oqs-sys/src/lib.rs
@@ -6,5 +6,8 @@ extern crate libc;
 #[allow(non_camel_case_types)]
 #[allow(non_snake_case)]
 pub mod kex {
+    pub static SUCCESS: ::libc::c_int = 1;
+    pub static FAILURE: ::libc::c_int = 0;
+
     include!(concat!(env!("OUT_DIR"), "/kex.rs"));
 }
