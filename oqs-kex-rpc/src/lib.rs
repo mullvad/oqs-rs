cc5cfd985a2335b7357e7cc7d9283376c839d8a6
Linus Färnstrand <linus@mullvad.net>
2017-09-26 19:29:12 +0200

    Add RPC server boilerplate

--- oqs-kex-rpc/src/lib.rs
+++ oqs-kex-rpc/src/lib.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate error_chain;
+
 extern crate oqs;
 extern crate uuid;
 
@@ -5,4 +8,10 @@ extern crate uuid;
 extern crate jsonrpc_client_core;
 extern crate jsonrpc_client_http;
 
+extern crate jsonrpc_core;
+extern crate jsonrpc_http_server;
+#[macro_use]
+extern crate jsonrpc_macros;
+
 pub mod client;
+pub mod server;
---
+++ oqs-kex-rpc/src/server/rpc.rs
@@ -0,0 +1,63 @@
+use uuid::Uuid;
+
+use oqs::kex::SharedKey;
+
+use std::net::SocketAddr;
+use std::str::FromStr;
+
+use jsonrpc_core::IoHandler;
+use jsonrpc_http_server::{self, ServerBuilder};
+
+mod api {
+    use jsonrpc_core::Error;
+    use oqs::kex::{AliceMsg, BobMsg, SharedKey};
+    use uuid::Uuid;
+
+    build_rpc_trait! {
+        pub trait OqsKexRpcServerApi {
+            #[rpc(name = "kex")]
+            fn kex(&self, Vec<AliceMsg>) -> Result<(Uuid, Vec<BobMsg>), Error>;
+        }
+    }
+
+    pub struct OqsKexRpcServer<F>
+    where
+        F: Fn(Uuid, Vec<SharedKey>) + Send + Sync + 'static,
+    {
+        pub on_kex: F,
+    }
+
+    impl<F> OqsKexRpcServerApi for OqsKexRpcServer<F>
+    where
+        F: Fn(Uuid, Vec<SharedKey>) + Send + Sync + 'static,
+    {
+        fn kex(&self, _alice_msgs: Vec<AliceMsg>) -> Result<(Uuid, Vec<BobMsg>), Error> {
+            unimplemented!();
+        }
+    }
+}
+use self::api::{OqsKexRpcServer, OqsKexRpcServerApi};
+
+error_chain! {
+    foreign_links {
+        AddrParseError(::std::net::AddrParseError);
+        HttpServerError(jsonrpc_http_server::Error);
+    }
+}
+
+/// Tries to start a HTTP JSON-RPC 2.0 server at `addr`. Will call `on_kex` after each finished
+/// negotiation and give it the uuid and the resulting keys of the key exchange.
+pub fn start<F>(addr: &str, on_kex: F) -> Result<jsonrpc_http_server::Server>
+where
+    F: Fn(Uuid, Vec<SharedKey>) + Send + Sync + 'static,
+{
+    let parsed_addr = SocketAddr::from_str(addr)?;
+
+    let server = OqsKexRpcServer { on_kex };
+    let mut io = IoHandler::new();
+    io.extend_with(server.to_delegate());
+
+    ServerBuilder::new(io)
+        .start_http(&parsed_addr)
+        .map_err(Error::from)
+}
