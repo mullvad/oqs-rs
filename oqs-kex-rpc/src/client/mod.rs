99d220399941f495ed38534630321bb8cec45888
Linus Färnstrand <linus@mullvad.net>
2017-10-06 17:59:43 +0200

    Add copyright header to all source files

--- oqs-kex-rpc/src/client/mod.rs
+++ oqs-kex-rpc/src/client/mod.rs
@@ -1,3 +1,11 @@
+// Copyright 2017 Amagicom AB.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use oqs;
 use oqs::kex::{AliceMsg, BobMsg, OqsKex, OqsKexAlg, OqsKexAlice, OqsRand, OqsRandAlg, SharedKey};
 use jsonrpc_client_http::HttpHandle;
