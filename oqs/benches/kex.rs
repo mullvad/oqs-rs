59c999e2946b3c8e427fda5a1eda753f3b2dec27
Linus Färnstrand <linus@mullvad.net>
2017-10-09 22:12:36 +0200

    Update kex benchmarks to new crate structure

--- oqs/benches/kex.rs
+++ oqs/benches/kex.rs
@@ -4,29 +4,16 @@ extern crate oqs;
 extern crate test;
 
 use test::Bencher;
-use oqs::kex::{OqsKex, OqsKexAlg, OqsRand, OqsRandAlg};
-
-#[bench]
-fn create_rand_default(b: &mut Bencher) {
-    b.iter(|| OqsRand::new(OqsRandAlg::Default));
-}
-
-#[bench]
-fn create_rand_urandom_chacha20(b: &mut Bencher) {
-    b.iter(|| OqsRand::new(OqsRandAlg::UrandomChacha20));
-}
-
-#[bench]
-fn create_rand_urandom_aesctr(b: &mut Bencher) {
-    b.iter(|| OqsRand::new(OqsRandAlg::UrandomAesctr));
-}
+use oqs::kex::{OqsKex, OqsKexAlg};
+use oqs::rand::{OqsRand, OqsRandAlg};
 
 macro_rules! bench_alice0 {
     ($name:ident, $algo:ident) => (
         #[bench]
         fn $name(b: &mut Bencher) {
+            let rand = OqsRand::new(OqsRandAlg::default()).unwrap();
+            let kex = OqsKex::new(&rand, OqsKexAlg::$algo).unwrap();
             b.iter(|| {
-                let kex = OqsKex::new(OqsRandAlg::default(), OqsKexAlg::$algo).unwrap();
                 kex.alice_0().unwrap()
             });
         }
@@ -41,18 +28,17 @@ macro_rules! bench_full_kex {
     ($name:ident, $algo:ident) => (
         #[bench]
         fn $name(b: &mut Bencher) {
+            let rand = OqsRand::new(OqsRandAlg::default()).unwrap();
             b.iter(|| {
-                let kex_alice = OqsKex::new(OqsRandAlg::default(), OqsKexAlg::$algo)
+                let kex_alice = OqsKex::new(&rand, OqsKexAlg::$algo).unwrap();
+                let kex_alice_0 = kex_alice.alice_0().unwrap();
+
+                let (bob_msg, shared_key1) = OqsKex::new(&rand, OqsKexAlg::$algo)
                     .unwrap()
-                    .alice_0()
+                    .bob(kex_alice_0.get_alice_msg())
                     .unwrap();
 
-                let (bob_msg, shared_key1) = OqsKex::new(OqsRandAlg::default(), OqsKexAlg::$algo)
-                    .unwrap()
-                    .bob(kex_alice.get_alice_msg())
-                    .unwrap();
-
-                let shared_key2 = kex_alice.alice_1(&bob_msg).unwrap();
+                let shared_key2 = kex_alice_0.alice_1(&bob_msg).unwrap();
                 (shared_key1, shared_key2)
             })
         }
